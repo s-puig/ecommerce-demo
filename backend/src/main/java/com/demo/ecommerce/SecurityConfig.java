7e016f59c80a68cf8142b934cbff882383986932
s-puig <39652109+s-puig@users.noreply.github.com>
2025-06-30 18:05:01 +0200

    Disable built-in authentification and CSRF (#1)
    Disable built-in authentificacion since we will use JWT tokens on our
    REST API.
    Disable CSRF since it is not useful in REST applications.

---
+++ backend/src/main/java/com/demo/ecommerce/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.demo.ecommerce;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // Disable the auth login since we are going to use JWT
+                .formLogin(AbstractHttpConfigurer::disable)
+                // Not needed in a REST API
+                .csrf(CsrfConfigurer::disable).build();
+    }
+}
