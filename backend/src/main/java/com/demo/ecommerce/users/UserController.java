caf973323d5605d2a720fa5fe1eddb2b0ecf8a3d
Sergi Puig <puig.sergi@outlook.com>
2025-07-06 22:10:44 +0200

    Add first unit test in User controller

--- backend/src/main/java/com/demo/ecommerce/users/UserController.java
+++ backend/src/main/java/com/demo/ecommerce/users/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "users", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "api/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
     @Autowired
     UserService userService;
--- backend/src/test/java/com/demo/ecommerce/users/UserControllerTest.java
+++ backend/src/test/java/com/demo/ecommerce/users/UserControllerTest.java
@@ -1,32 +1,38 @@
 package com.demo.ecommerce.users;
 
+import com.demo.ecommerce.common.annotations.ImportTestContext;
 import com.demo.ecommerce.users.dto.UserPublicData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+/*import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;*/
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 
 import java.util.EnumSet;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Tag("Unit")
-@Import(UserMapperImpl.class)
+@ImportTestContext(UserMapperImpl.class)
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockitoBean
     private UserService userService;
-
-    /*@MockitoBean
-    private UserMapper userMapper;*/
 
     @Test
     void testController() {
@@ -34,7 +40,7 @@ public class UserControllerTest {
     }
 
     @Test
-    void get_returnUserPublic() {
+    void get_returnUserPublic() throws Exception {
         Long userId = 10L;
         String name = "Test";
         String email = "test@outlook.com";
@@ -44,6 +50,8 @@ public class UserControllerTest {
 
         when(userService.find(10L)).thenReturn(Optional.of(user));
 
-        mockMvc.perform(get("/users/{id}", userId))
+        mockMvc.perform(get("/api/users/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(userDto)));
     }
 }
