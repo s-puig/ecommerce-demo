e986f0af8fd8efb80abc0067d36be4a8ecef00ec
Sergi Puig <puig.sergi@outlook.com>
2025-07-14 17:33:48 +0200

    Add product repository

--- backend/src/main/java/com/demo/ecommerce/products/ProductRepository.java
+++ backend/src/main/java/com/demo/ecommerce/products/ProductRepository.java
@@ -1,7 +1,7 @@
-package com.demo.ecommerce.users;
+package com.demo.ecommerce.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
