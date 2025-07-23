a698959fd50014b6618dd1b490c1f648758261be
S. Puig <puig.sergi@outlook.com>
2025-07-23 22:23:13 +0200

    Add documentation for UserMapper, UserController and UserService.

--- backend/src/main/java/com/demo/ecommerce/users/UserMapper.java
+++ backend/src/main/java/com/demo/ecommerce/users/UserMapper.java
@@ -7,7 +7,27 @@ import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    /**
+     * Maps a {@link User} entity to a {@link UserPublicData} DTO.
+     *
+     * @param user the {@link User} object containing user details to be mapped
+     * @return a new {@link UserPublicData} object initialized with data from the {@link User} entity
+     */
     UserPublicData entityToResponse(User user);
+    /**
+     * Maps a {@link UserCreate} DTO to a {@link User} entity.
+     *
+     * @param user the {@link UserCreate} object containing user details to be mapped
+     * @return a new {@link User} entity initialized with data from the {@link UserCreate} DTO
+     */
     User createToEntity(UserCreate user);
+    /**
+     * Updates a {@link User} entity with data from a {@link UserCreate} DTO.
+     * This method maps the fields from the provided {@link UserCreate} object to the target {@link User}
+     * entity.
+     *
+     * @param userCreate The {@link UserCreate} object containing the new data for the user.
+     * @param user       The target {@link User} entity to be updated with new values from the DTO.
+     */
     void updateUser(UserCreate userCreate, @MappingTarget User user);
 }
